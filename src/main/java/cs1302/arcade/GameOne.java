306144637928925de14924ef263ceb0196560ba1
Matthew Funkhouser <mwf59348@uga.edu>
2019-11-12 16:13:20 -0500

    Made the menu and animation for the opening of the program

---
+++ src/main/java/cs1302/arcade/GameOne.java
@@ -0,0 +1,36 @@
+package cs1302.arcade;
+
+import java.util.Random;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.animation.Timeline;
+import javafx.animation.KeyFrame;
+import java.io.IOException;
+import java.util.ArrayList;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class GameOne extends Group {
+
+    public GameOne() {
+
+
+    }
+    
+}
