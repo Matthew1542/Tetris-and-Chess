0c777bf2a25c55eebc9fa69a467419dc7cab12e4
Jess Hill <jbh41014@uga.edu>
2019-11-20 14:54:44 -0500

    started the tetris game

--- src/main/java/cs1302/arcade/GameOne.java
+++ src/main/java/cs1302/arcade/GameOne.java
@@ -26,10 +26,27 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
+/**
+ * This class is a javaFX made game of tetris
+ *
+ */
 public class GameOne extends Group {
 
-    public GameOne() {
+    /** size of each block */
+    public final int size = 36;
 
+    /** size of the width of the game board */
+    public final int xSize = size * 10;
+
+    /** height of the game board */
+    public final int ySize = xSize * 2;
+
+    /** double array containing every possible spot for each block */
+    public final int[][] grid = new int[xSize / size][ySize / size];
+    
+
+    public GameOne() {
+	
 
     }
     
