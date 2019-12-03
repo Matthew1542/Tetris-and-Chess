2c28cff00107b904382f2f422248482def841772
Matthew Funkhouser <mwf59348@uga.edu>
2019-12-02 20:05:39 -0500

    Put the pieces on the board in chess and started tracking mouse input

--- src/main/java/cs1302/arcade/GameTwo.java
+++ src/main/java/cs1302/arcade/GameTwo.java
@@ -44,22 +44,10 @@ public class GameTwo {
 
     /** Image to be used as the game board. */
     ImageView board = new ImageView(new Image("file:resources/chessBoard.png"));
-    Image blackPawn = new Image("file:resources/blackPawn.png"); //pawn
-    Image whitePawn = new Image("file:resources/whitePawn.png"); //pawn
-    Image blackRook = new Image("file:resources/blackRook.png"); //rook
-    Image whiteRook = new Image("file:resources/whiteRook.png"); //rook
-    Image blackBishop = new Image("file:resources/blackBishop.png"); //bishop
-    Image whiteBishop = new Image("file:resources/whiteBishop.png"); //bishop
-    Image blackQueen = new Image("file:resources/blackQueen.png"); //queen
-    Image whiteQueen = new Image("file:resources/whiteQueen.png"); //queen
-    Image blackKing = new Image("file:resources/blackKing.png"); //king
-    Image whiteKing = new Image("file:resources/whiteKing.png"); //king
-    Image blackKnight = new Image("file:resources/blackKnight.png"); //knight
-    Image whiteKnight = new Image("file:resources/whiteKnight.png"); //knight
     Image posRoute = new Image("file:resources/pos.png"); //highlights routes
-    
 
-    int score = 40; //score goes down with each loss of a piece
+
+    int score = 100; //score goes down with each loss of a piece
     Group group = new Group();
     ArcadeApp app;
     Scene scene;
@@ -67,6 +55,13 @@ public class GameTwo {
     Text score2Text;
 
     Boolean turn = true; //which turn it is
+
+    ArrayList<Piece> pieces = new ArrayList<Piece>();
+    
+    String[] typeArray = {"bR", "bH", "bB", "bQ", "bK", "bB", "bH", "bR",
+        "bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP",
+        "wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP",
+        "wR", "wH", "wB", "wK", "wQ", "wB", "wH", "wR"};
     
     /**
      * The constructor of the chess game.
@@ -74,43 +69,61 @@ public class GameTwo {
      * @param app The parent {@code ArcadeApp} used for scene-change
      */
     public GameTwo(ArcadeApp app) {
-	this.app = app;
-	scene = new Scene(group, 1280, 720);
-	for (int i = 0; i < 8; i++) {
-	    for (int x = 0; x < 8; x++) {
-		if (i == 0 || i == 1) {
-		    grid[i][x] = 1; //1 = white piece
-		} else if (i == 6 || i == 7) {
-		    grid[i][x] = 2; //2 = black piece
-		} else {
-		    //if you are here, the spot is empty
-		    grid[i][x] = 0; //empty spot
-		} //else		
-	    } //for	    
-	} //for
+        this.app = app;
+        group.getChildren().add(board);
+        scene = new Scene(group, 1280, 720);
+        for (int i = 0; i < 8; i++) {
+            for (int x = 0; x < 8; x++) {
+                if (i == 0 || i == 1) {
+                    grid[i][x] = 1; //1 = white piece
+                } else if (i == 6 || i == 7) {
+                    grid[i][x] = 2; //2 = black piece
+                } else {
+                    //if you are here, the spot is empty
+                    grid[i][x] = 0; //empty spot
+                } //else
+            } //for
+        } //for
+        for (int j = 0; j < 4; j++) {
+            for (int i = 0; i < 8; i++) {
+                int x = 280 + (i * 90);
+                int y = j * 90 + 360;
+                if (j < 2) {
+                    y = j * 90;
+                } 
+                String type = typeArray[j * 8 + i];
+                Piece p = new Piece(x, y, type);
+                
+                pieces.add(p);
+            }
+        }
+        
+        for (int i = 0; i < pieces.size(); i++)  {
+            group.getChildren().add(pieces.get(i));
+        }
 
-	score1Text = new Text("" + score);
-	score1Text.setX(1105);
-	score1Text.setY(175);
-	score1Text.setFont(Font.font("Verdana", FontWeight.BOLD, 40));
+        score1Text = new Text("" + score);
+        score1Text.setX(1105);
+        score1Text.setY(175);
+        score1Text.setFont(Font.font("Verdana", FontWeight.BOLD, 40));
 
-	score2Text = new Text("" + score);
+        score2Text = new Text("" + score);
         score2Text.setX(115);
         score2Text.setY(175);
         score2Text.setFont(Font.font("Verdana", FontWeight.BOLD, 40));
 
-	Button quit = new Button("Quit");
-	quit.setTranslateX(10);
-	quit.setTranslateY(690);
-	quit.setOnAction(new EventHandler<ActionEvent>() {
-		@Override public void handle(ActionEvent e) {
-		    app.gameState = "MENU";
-		    app.updateScene();
-		    //stop timeline
-		}
-	    });
-	group.getChildren().addAll(board, quit, score1Text, score2Text);
-	
+        Button quit = new Button("Quit");
+        quit.setTranslateX(10);
+        quit.setTranslateY(690);
+        quit.setOnAction(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent e) {
+                    app.gameState = "MENU";
+                    app.updateScene();
+                }
+            });
+        group.getChildren().addAll(quit, score1Text, score2Text);
+        group.setOnMouseClicked(createMouseHandler()); 
+
     } //gameTwo
 
     /**
@@ -119,9 +132,40 @@ public class GameTwo {
      * @return the scene
      */
     public Scene getScene() {
-	return scene;
+        return scene;
     } //getter
-
-
     
+    private EventHandler<? super MouseEvent> createMouseHandler() {
+        return event -> {
+            int x = (int)event.getX();
+            int y = (int)event.getY();
+            x = (int)Math.floor((x - 280) / 90);
+            y = (int)Math.floor(y / 90);
+            System.out.println("X: " + x + " Y: " + y);
+            for (int i = 0; i < pieces.size(); i++) {
+                if ((int)Math.floor((pieces.get(i).getX() - 280) / 90) == x &&
+                    (int)Math.floor(pieces.get(i).getY() / 90) == y) {
+                    if (type.equals("wP")) {
+                        moveWhitePawn(pieces.get(i));
+                    } else if (type.equals("bP")) {
+                        moveBlackPawn(pieces.get(i));
+                    } else if (type.contains("R")) {
+                        moveRook(pieces.get(i));
+                    } else if (type.contains("H")) {
+                        moveHorse(pieces.get(i));
+                    } else if (type.contains("B")) {
+                        moveBishop(pieces.get(i));
+                    } else if (type.contains("K")) {
+                        moveKing(pieces.get(i));
+                    } else if (type.contains("Q")) {
+                        moveQueen(pieces.get(i));
+                    }
+                    System.out.println(pieces.get(i).type);
+                }
+            }
+        };
+    } // createMouseHandler
+
+
+
 }
--- src/main/java/cs1302/arcade/Piece.java
+++ src/main/java/cs1302/arcade/Piece.java
@@ -5,30 +5,68 @@ import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 
-String type; //the type of piece to be used
-ImagePattern png; //the image of the piece 
-    
+
 /**
  * The pieces that are being used in chess.
  *
  */
-public class Piece extends Rectangle {
+public class Piece extends Rectangle{
 
-    public Rectangle(int x, int y, String type) {
-	this.setX(x);
-	this.setY(y);
-	this.type = type;
-	
+    String type; //the type of piece to be used
+    ImagePattern png; //the image of the piece
+    Rectangle r;
+
+    public Piece(int x, int y, String type) {
+        super(90, 90);
+        this.setX(x);
+        this.setY(y);
+        this.r = r;
+        this.type = type;
+        setImage(type);
     } //constructor
 
     public void setImage(String type) {
-	if (type.equals("WP")) {
-	    this.png = new ImagePattern(new Image("file:resources/
-	}
+        if (type.equals("wP")) {
+            this.png = new ImagePattern(new Image("file:resources/whitePawn.png"));
+        } //if
+        if (type.equals("bP")) {
+            this.png = new ImagePattern(new Image("file:resources/blackPawn.png"));
+        } //if
+        if (type.equals("wR")) {
+            this.png = new ImagePattern(new Image("file:resources/whiteRook.png"));
+        } //if
+        if (type.equals("bR")) {
+            this.png = new ImagePattern(new Image("file:resources/blackRook.png"));
+        } //if
+        if (type.equals("wK")) {
+            this.png = new ImagePattern(new Image("file:resources/whiteKing.png"));
+        } //if
+        if (type.equals("bK")) {
+            this.png = new ImagePattern(new Image("file:resources/blackKing.png"));
+        } //if
+        if (type.equals("wH")) {
+            this.png = new ImagePattern(new Image("file:resources/whiteKnight.png"));
+        } //if
+        if (type.equals("bH")) {
+            this.png = new ImagePattern(new Image("file:resources/blackKnight.png"));
+        } //if
+        if (type.equals("wQ")) {
+            this.png = new ImagePattern(new Image("file:resources/whiteQueen.png"));
+        } //if
+        if (type.equals("bQ")) {
+            this.png = new ImagePattern(new Image("file:resources/blackQueen.png"));
+        } //if
+        if (type.equals("wB")) {
+            this.png = new ImagePattern(new Image("file:resources/whiteBishop.png"));
+        } //if
+        if (type.equals("bB")) {
+            this.png = new ImagePattern(new Image("file:resources/blackBishop.png"));
+        } //if
+        this.setFill(this.png);
     } //setter
-    
 
-    
+
+
 
 
 }
